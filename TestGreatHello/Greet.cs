cfc16d2a1d5531346f8fa3bc0af1c47865613157
laureato miraglia <tatomball96@gmail.com>
2021-11-12 10:38:11 +0100

    miragliaLaureato
    .

--- TestGreatHello/Greet.cs
+++ TestGreatHello/Greet.cs
@@ -1,39 +1,67 @@
-﻿using System.Linq;
-
+﻿using System.Collections.Generic;
+using System.Linq;
 namespace TestGreatHello
+
 {
     public class Greet : IGreetHello
     {
-        private static string[] SplitComma(string sign, string[] names)
-        {
-            var comma = names?.Where(x => x.Contains(sign)).ToArray();
-            names = names?.Except(comma).ToArray();
-            return names?.Concat(comma.SelectMany(x => x.Split(sign))).ToArray();
-        }
-
+      
         public string GreetHello(params string[] names)
         {
-            names = SplitComma(", ", names);
-
             if (names == null)
+            {
                 return "Hello, my friend.";
-
+            }
             if (names.Length == 1)
+            {
                 return names[0] == names[0].ToUpper() ? $"HELLO {names[0]}!" : $"Hello, {names[0]}.";
+            }
+            if (names.Length == 2 && !names[0].Contains(",") && !names[1].Contains(","))
+            {
+                return "Hello, " + names[0] + " and " + names[1] + ".";
+            }
 
-            if (names.Length == 2)
-                return $"Hello, {names[0]} and {names[1]}.";
+            bool trovato = false; 
+            List<string> nomiUp = new();
+            List<string> nomiDown = new();
+            foreach (var tmp in names)
+            {
+                if (tmp.All(char.IsUpper))
+                {
+                    nomiUp.Add(tmp);
+                }
+                else
+                if (tmp.Contains(",") && tmp.Contains('"'))
+                {
+                    trovato = true; 
+                    nomiDown.Add(string.Concat(" and", tmp.Replace('"',' ').Split(",").First(),',', tmp.Replace("\"", string.Empty).Split(",").Last()));
+                }
+                else
+                if (tmp.Contains(","))
+                {
+                    nomiDown.AddRange(tmp.Split(','));
+                }
+                else
+                {
+                    nomiDown.Add(tmp);
+                }
 
-            var nameUpp = names.Where(x => x.All(char.IsUpper)).ToList();
-            var nameDown = names.Except(nameUpp).ToList();
+            }
 
-            var result = string.Concat("Hello, ", string.Join(", ", nameDown.Where(x => x != nameDown.Last())), " and ", nameDown.Last(), ".");
+            if (nomiUp.Any())
+            {
+                return string.Concat("Hello, ", string.Join(", ", nomiDown.Where(x => x != nomiDown.Last())),
+                    " and ", nomiDown.Last(), ". AND HELLO ", string.Join("", nomiUp), "!");
+            }
+            if (trovato)
+            {
+                return string.Concat("Hello, ", string.Join(nomiDown.First(),nomiDown.Where(x => x != nomiDown.Last())),nomiDown.Last(), ".");
+            }
             
-            if (nameUpp.Any())
-                return string.Concat(result, " AND HELLO ", string.Join(", ", nameUpp.Select(x => x)), "!");
-
-            return result;
+            return string.Concat("Hello, ", string.Join(", ", nomiDown.Where(x => x != nomiDown.Last())), " and ", nomiDown.Last(), ".");
         }
+
+
 
     }
 }
